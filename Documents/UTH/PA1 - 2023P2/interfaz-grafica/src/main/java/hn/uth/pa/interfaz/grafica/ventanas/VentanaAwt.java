6ffe27de1bc1f64ad9e5861925c2ca50f990a564
omaldonado22 <orlynm10@gmail.com>
2023-05-30 10:12:00 -0600

    Si

---
+++ Documents/UTH/PA1 - 2023P2/interfaz-grafica/src/main/java/hn/uth/pa/interfaz/grafica/ventanas/VentanaAwt.java
@@ -0,0 +1,15 @@
+
+package hn.uth.pa.interfaz.grafica.ventanas;
+import java.awt.Frame;
+
+public class VentanaAwt extends Frame 
+{
+
+    public VentanaAwt()  {
+        super();
+        this.setTitle("Ventana de la APP");
+        this.setSize(400,400);
+        this.setVisible(true);
+    }
+    
+}
