14b2714202202a3629cb87d6aaa4339ed824736a
omaldonado22 <orlynm10@gmail.com>
2023-05-30 11:02:16 -0600

    no

---
+++ Documents/UTH/mavenproject4/src/main/java/hn/uth/pa/mavenproject4/Mavenproject4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package hn.uth.pa.mavenproject4;
+
+/**
+ *
+ * @author orlyne.maldonado
+ */
+public class Mavenproject4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
